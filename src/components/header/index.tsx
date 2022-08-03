5427f6035d038f41a01273629673da8f3336366d
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-02 22:06:44 -0300

    feat(components): style header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,15 +7,15 @@ import {
   MenuItem,
   IconButton,
   Button,
-  MenuItemOption,
   MenuGroup,
-  MenuOptionGroup,
   MenuDivider,
+  useDisclosure,
 } from "@chakra-ui/react";
 
-import { AddIcon } from "@chakra-ui/icons";
+import { AddIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
 const Header = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <HStack
       w="full"
@@ -24,27 +24,49 @@ const Header = () => {
       paddingY="4"
       paddingX={["4", "4", "12"]}
     >
-      <Heading ml="3" size="lg" color="yellow.600" fontWeight="700">
+      <Heading ml="3" size="xl" color="yellow.600" fontWeight="700">
         TMDB
       </Heading>
       <Menu>
-        <MenuButton as={Button}>Actions</MenuButton>
-        <MenuList>
-          <MenuButton
-            as={IconButton}
-            aria-label="Lists"
-            icon={<AddIcon />}
-            variant="outline"
-          >
-            Listas
-          </MenuButton>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          px={4}
+          py={2}
+          transition="all 0.2s"
+          borderRadius="md"
+          borderWidth="1px"
+          borderColor="yellow.700"
+          bg="gray.700"
+          color="yellow.700"
+          _hover={{ bg: "gray.500" }}
+          _expanded={{ bg: "gray.400" }}
+          _focus={{ boxShadow: "outline" }}
+          onClick={onOpen}
+        >
+          Actions
+        </MenuButton>
+        {isOpen ? (
+          <MenuList>
+            <MenuItem
+              icon={<AddIcon />}
+              as={Button}
+              aria-label="Lists"
+              // leftIcon={<AddIcon />}
+              onClick={onClose}
+            >
+              Criar Lista
+            </MenuItem>
 
-          <MenuDivider />
-          <MenuGroup title="Minhas listas">
-            <MenuItem>My Account</MenuItem>
-            <MenuItem>Payments </MenuItem>
-          </MenuGroup>
-        </MenuList>
+            <MenuDivider />
+            <MenuGroup title="Minhas listas">
+              <MenuItem>My Account</MenuItem>
+              <MenuItem>Payments </MenuItem>
+            </MenuGroup>
+          </MenuList>
+        ) : (
+          <></>
+        )}
       </Menu>
     </HStack>
   );
