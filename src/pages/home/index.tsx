cdff504dbc72c72315446582fdd89ad461306edf
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-04 15:30:29 -0300

    feat(home): create more section home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,21 +1,43 @@
 import { Button, Flex, Heading, Input, Text, VStack } from "@chakra-ui/react";
+
+import { useContext, useEffect } from "react";
+
 import Header from "../../components/header";
+import SectionHome from "../../components/sectionsHome";
+
+import { FiTrendingUp } from "react-icons/fi";
+import { MdLocalMovies, MdMovie } from "react-icons/md";
+
+import { TmdbContext } from "../../providers/context";
 
 const Home = () => {
+  const { SectionInital, AllListsHome, load, trend, movies, tvSeries } =
+    useContext(TmdbContext);
+
+  useEffect(() => {
+    SectionInital();
+    AllListsHome();
+  }, []);
+
   return (
-    <Flex flexDir="column">
+    <Flex flexDir="column" w="100%">
       <Header />
       <Flex
         flexDir="column"
         mt={0}
-        bg="yellow.800"
         w="100%"
         p={6}
         color="white"
         gap={2}
+        py={8}
+        h="360px"
+        justifyContent="center"
+        bgGradient="linear(to-l, #fff350, #ffea44, #ffe139, #ffd72c, #ffce1f, #fac618, #f6bd0f, #f1b505, #e6ac04, #dca302, #d19a01, #c79100),url(https://image.tmdb.org/t/p/w1920/uozb2VeD87YmhoUP1RrGWfzuCrr.jpg);"
+        bgPosition="center"
+        bgRepeat="no-repeat"
       >
-        <Heading size="2xl">Bem vindo(a)!</Heading>
-        <Text fontSize="xl">Encontre e Organize suas séries e filmes</Text>
+        <Heading size="3xl">Bem vindo(a)!</Heading>
+        <Text fontSize="2xl">Encontre e Organize suas séries e filmes</Text>
         <Flex mt={3} bg="white" borderRadius="full">
           <Input
             bg="white"
@@ -26,16 +48,34 @@ const Home = () => {
           />
           <Button
             borderRadius="full"
-            bg="yellow.600"
+            bgGradient="linear(to-r, #fff350, #ffea44, #ffe139, #ffd72c, #ffce1f, #fac618, #f6bd0f, #f1b505, #e6ac04, #dca302, #d19a01, #c79100)"
             color="gray.600"
             paddingX={5}
+            _hover={{ color: "white" }}
           >
             Pesquisar
           </Button>
         </Flex>
       </Flex>
-      <VStack mt={4} alignItems="self-start" p={4}>
-        <Heading size="lg">Têndencias</Heading>
+      <VStack spacing={3} bg="gray.100">
+        <SectionHome
+          load={load}
+          title="Tendências"
+          trend={trend}
+          icon={FiTrendingUp}
+        />
+        <SectionHome
+          load={load}
+          title="Filmes"
+          trend={movies}
+          icon={MdLocalMovies}
+        />
+        <SectionHome
+          load={load}
+          title="Series"
+          trend={tvSeries}
+          icon={MdMovie}
+        />
       </VStack>
     </Flex>
   );
