a172d61f43b833e3cf629592d62186e93595a931
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-05 00:17:36 -0300

    feat(home):update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Flex, Heading, Input, Text, VStack } from "@chakra-ui/react";
 
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import Header from "../../components/header";
 import SectionHome from "../../components/sectionsHome";
@@ -9,13 +9,28 @@ import { FiTrendingUp } from "react-icons/fi";
 import { MdLocalMovies, MdMovie } from "react-icons/md";
 
 import { TmdbContext } from "../../providers/context";
+import { useHistory } from "react-router-dom";
 
 const Home = () => {
-  const { SectionInital, AllListsHome, load, trend, movies, tvSeries } =
-    useContext(TmdbContext);
+  const {
+    SectionInital,
+    AllListsHome,
+    load,
+    trend,
+    movies,
+    tvSeries,
+    getSession,
+  } = useContext(TmdbContext);
+
+  const history = useHistory();
+
+  const [seach, setSearch] = useState("");
 
   useEffect(() => {
-    SectionInital();
+    if (!getSession) {
+      SectionInital();
+    }
+
     AllListsHome();
   }, []);
 
@@ -32,9 +47,7 @@ const Home = () => {
         py={8}
         h="360px"
         justifyContent="center"
-        bgGradient="linear(to-l, #fff350, #ffea44, #ffe139, #ffd72c, #ffce1f, #fac618, #f6bd0f, #f1b505, #e6ac04, #dca302, #d19a01, #c79100),url(https://image.tmdb.org/t/p/w1920/uozb2VeD87YmhoUP1RrGWfzuCrr.jpg);"
-        bgPosition="center"
-        bgRepeat="no-repeat"
+        bgGradient="linear(to-l, #fff350, #ffea44, #ffe139, #ffd72c, #ffce1f, #fac618, #f6bd0f, #f1b505, #e6ac04, #dca302, #d19a01, #c79100);"
       >
         <Heading size="3xl">Bem vindo(a)!</Heading>
         <Text fontSize="2xl">Encontre e Organize suas séries e filmes</Text>
@@ -45,6 +58,8 @@ const Home = () => {
             borderRadius="full"
             placeholder="Busque um filme ou serie"
             color="gray.700"
+            value={seach}
+            onChange={(e) => setSearch(e.target.value)}
           />
           <Button
             borderRadius="full"
@@ -52,6 +67,7 @@ const Home = () => {
             color="gray.600"
             paddingX={5}
             _hover={{ color: "white" }}
+            onClick={() => history.push(`search/${seach}`)}
           >
             Pesquisar
           </Button>
