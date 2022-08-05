4444e357b5274cac26a1985a6bc426e40b6bb653
emillyalmeida <emillyalmeidan@gmail.com>
2022-08-04 22:04:52 -0300

    feat(route): create route search and add hook-form

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,11 +1,15 @@
 import { Switch, Route } from "react-router-dom";
 import Home from "../pages/home";
+import Search from "../pages/search";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/search/:query">
+        <Search />
       </Route>
     </Switch>
   );
